2042ae5de43b4256ce19243fd17c5aa8fd86e2cc
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-04-20 03:26:47 +0530

    Add Content Security Policy headers in Next.js config; update package dependencies for Supabase and related libraries

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const WaitListPage = () => {
+    return (
+        <div className="mt-20 fixed w-full">
+            <div className="aspect-video w-full">
+                <iframe style={{width : "100%"}}
+                    height="730"
+                    src="https://vahiqllist.created.app"
+                    title="Waiting List Landing Page"
+                    frameBorder="0">
+                </iframe>
+            </div>
+        </div>
+    )
+}
+
+export default WaitListPage
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/embed",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-src 'self' https://vahiqllist.created.app/;",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
 
 export default nextConfig;
