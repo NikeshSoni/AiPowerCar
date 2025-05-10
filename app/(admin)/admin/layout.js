1498864dfbfaa81551c335a0b6520df9649614a7
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-05-11 02:19:38 +0530

    Implement admin authorization check in AdminLayout; integrate Headers component for improved layout structure.

--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { getAdmin } from "@/actions/admin"
+import { notFound } from 'next/navigation';
+import Headers from "@/components/header"
 
 const AdminLayout = async () => {
+
+  const admin = await getAdmin()
+
+  if (!admin.authorized) {
+    return notFound()
+  }
   return (
-    <div>
-         Hiiiii
+    <div className='h-full'>
+      <Headers isAdminPage={true} />
     </div>
   )
 }
