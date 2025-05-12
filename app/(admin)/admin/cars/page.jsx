4beeeca704d5a7ae06eb4196d47e0df810b86d58
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-05-13 05:27:47 +0530

    Refactor AdminLayout to enhance navigation by integrating Sidebar component; temporarily comment out admin authorization check for future implementation.

---
+++ app/(admin)/admin/cars/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import CarsList from './create/_components/car-list'
+
+
+export const metadata = {
+    title: "Cars | Nike Admin",
+    description: "Manage cars in your marketplace"
+}
+
+const CarsPage = () => {
+    return (
+        <div className='p-6'>
+            <h1 className="text-2xl font-bold mb-6 ">
+                Cars Management
+            </h1>
+
+            <CarsList />
+        </div>
+    )
+}
+
+export default CarsPage
